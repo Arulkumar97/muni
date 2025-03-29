2c829fd88984d90eb50a0f823e2a0ecb60cd9d27
Arulkumar97 <aruljee1570@gmail.com>
2025-03-29 17:45:35 +0530

    muni

---
+++ src/org/emp/EmployeeDetails.java
@@ -0,0 +1,32 @@
+package org.emp;
+
+public class EmployeeDetails {
+	private void empId() {
+		System.out.println("emp id 1570");
+	}
+	private void empName() {
+		System.out.println("emp name Arul");
+	}
+	private void empDob() {
+		System.out.println("emp dob 08/06/1997");
+	}
+	private void empPhone() {
+		System.out.println("emp phone 7845758317");
+	}
+	private void empEmail() {
+		System.out.println("emp Email aruljee1570@gmail.com");
+	}
+	private void empAddress() {
+		System.out.println("emp Address Namakkal");
+	}
+	public static void main(String[]args) {
+		EmployeeDetails c= new EmployeeDetails();
+		c.empId();
+		c.empName();
+		c.empDob();
+		c.empPhone();
+		c.empEmail();
+		c.empAddress();
+	}
+
+}
